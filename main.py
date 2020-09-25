cb1219dfe919b21482dc284dea5f63f7a21c4d19
Feroz <fk72074@gmail.com>
2020-09-25 14:57:01 +0500

    First commit

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 from subprocess import run, PIPE
-
+import logging
+import speech_recognition as sr
+import wave
+import contextlib
 from flask import logging, Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/upload')
+def upload():
+	filename = "audio.wav"
+	r=sr.Recognizer()
+	with sr.AudioFile(filename) as source:
+		audio_data = r.record(source)
+		text = r.recognize_google(audio_data)
+	with contextlib.closing(wave.open(filename,'r')) as f:
+		frames = f.getnframes()
+		rate = f.getframerate()
+		duration = frames / float(rate)
+
+	res = len(text.split())/duration #!pip install gTTS pyttsx3 playsound
+	res = res*60
+	data="words per minutes are "+ str(res)
+	return render_template('upload.html',wpm=' Prediction {}'.format(data))
+	#return "Total Wpm are" + data
+
 
 @app.route('/audio', methods=['POST'])
 def audio():
-    with open('/tmp/audio.wav', 'wb') as f:
+    with open('audio.wav', 'wb') as f:
         f.write(request.data)
-    proc = run(['ffprobe', '-of', 'default=noprint_wrappers=1', '/tmp/audio.wav'], text=True, stderr=PIPE)
+    proc = run(['ffprobe', '-of', 'default=noprint_wrappers=1', '/audio.wav'], text=True, stderr=PIPE)
     return proc.stderr
+
+@app.route('/read')
+def read():
+    file= "audio.wav"
+    #yaha code likh
+
 
 
 if __name__ == "__main__":
-    app.logger = logging.getLogger('audio-gui')
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,18 +1,3 @@
-/* Copyright 2013 Chris Wilson
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 var audioContext = new AudioContext();
@@ -41,7 +26,7 @@ function doneEncoding(soundBlob) {
 function stopRecording() {
     // stop recording
     audioRecorder.stop();
-    document.getElementById('stop').disabled = true;
+    document.getElementById('stop').disabled = false;
     document.getElementById('start').removeAttribute('disabled');
     audioRecorder.getBuffers(gotBuffers);
 }
@@ -126,6 +111,10 @@ function toggleMono() {
 
 function gotStream(stream) {
     document.getElementById('start').removeAttribute('disabled');
+    audioContext.resume().then(() => {
+        console.log('Playback resumed successfully');
+    });
+
 
     inputPoint = audioContext.createGain();
 
